f3699cc8a24c70b6591e0277e68b731fa243fee9
Darshit13J <darshitdamania@hotmail.com>
2025-04-18 16:58:04 +0530

    Update calculator.py
    Edit1

--- calculator.py
+++ calculator.py
@@ -4,21 +4,17 @@ class Calculator:
 
     def add(self, a, b):
         return a + b
-<<<<<< feature/sqrt
 
     def subtract(self, a, b):
         return a - b
+    def subtract(self, a, b):
+        return a - b
 
-=======
+    def multiply(self, a, b):
+        return a * b
 
-def subtract(self, a, b):
-    return a - b
-
-def multiply(self, a, b):
-    return a * b
-
-def divide(self, a, b):
-    return a / b
+    def divide(self, a, b):
+        return a / b
 
 
     def subtract(self, a, b):
